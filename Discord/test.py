96ca6e4abf0e5246e537f8e369b904c0bf8b66b4
Detropat <detropat@gmail.com>
2021-03-09 06:48:21 +0100

    Current status

--- Discord/test.py
+++ Discord/test.py
@@ -10,12 +10,21 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    print('Message called back: ', message.content)
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    # print('Message called back:', message.content, 'coming from', message.channel)
+
+    if message.channel.name == 'reservations-wednesday':
+        channel = message.channel
+        # print('Great, this message is coming from the reservations channel')
+        await channel.send(f'Thank you, {message.author.name}, we will process your reservation!')
+        if message.content.startswith('$reserve'):
+            print('Someone is trying to reserve', message.content)
+        elif message.content.startswith('$unreserve'):
+            print('Unreserving nation', message.content)
+        else:
+            print('Not much happening')
 
 
-client.run('ODE4NTc5MzQ5ODI0ODY0MjU2.YEaHbQ.VV7iPFb5leqewiCT0dW2VzMRTs0')
+client.run('ODE4NTc5MzQ5ODI0ODY0MjU2.YEaHbQ.bUmDG--0HZH1TA_0KQl96_gImIc')
